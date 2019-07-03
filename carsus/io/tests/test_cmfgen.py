1e101ec3403e822373a07670a507b7574f0d345a
epassaro <epassaro15@gmail.com>
2019-07-03 17:00:37 -0300

    Passing refdata through command line option

--- carsus/io/tests/test_cmfgen.py
+++ carsus/io/tests/test_cmfgen.py
@@ -9,48 +9,59 @@ from carsus.io.cmfgen import (CMFGENEnergyLevelsParser,
                               CMFGENPhotoionizationCrossSectionParser,
                              )
 
+with_refdata = pytest.mark.skipif(
+    not pytest.config.getoption("--refdata"),
+    reason="--refdata folder not specified"
+)
 
-REF_DATA_DIR = os.getenv('CARSUS_REFDATA')
-
+@with_refdata
 @pytest.fixture()
-def si2_osc_kurucz_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'energy_levels', 'si2_osc_kurucz')
+def si2_osc_kurucz_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'energy_levels', 'si2_osc_kurucz')
 
+@with_refdata
 @pytest.fixture()
-def fevi_osc_kb_rk_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'oscillator_strengths', 'fevi_osc_kb_rk.dat')
+def fevi_osc_kb_rk_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'oscillator_strengths', 'fevi_osc_kb_rk.dat')
 
+@with_refdata
 @pytest.fixture()
-def p2_osc_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'oscillator_strengths', 'p2_osc')
+def p2_osc_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'oscillator_strengths', 'p2_osc')
 
+@with_refdata
 @pytest.fixture()
-def vi_osc_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'oscillator_strengths', 'vi_osc')
+def vi_osc_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'oscillator_strengths', 'vi_osc')
 
+@with_refdata
 @pytest.fixture()
-def he2_col_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'collisional_data', 'he2col.dat')
+def he2_col_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'collisional_data', 'he2col.dat')
 
+@with_refdata
 @pytest.fixture()
-def ariii_col_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'collisional_data', 'col_ariii')
+def ariii_col_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'collisional_data', 'col_ariii')
 
+@with_refdata
 @pytest.fixture()
-def si2_pho_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'photoionization_cross_sections', 'phot_nahar_A')
+def si2_pho_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'photoionization_cross_sections', 'phot_nahar_A')
 
+@with_refdata
 @pytest.fixture()
-def coiv_pho_fname():
-    return os.path.join(REF_DATA_DIR, 'cmfgen', 'photoionization_cross_sections', 'phot_data_gs')
+def coiv_pho_fname(refdata_path):
+    return os.path.join(refdata_path, 'cmfgen', 'photoionization_cross_sections', 'phot_data_gs')
 
-
+@with_refdata
 def test_si2_osc_kurucz(si2_osc_kurucz_fname):
     parser = CMFGENEnergyLevelsParser(si2_osc_kurucz_fname)
     n = int(parser.meta['Number of energy levels'])
     assert parser.base.shape[0] == n
     assert parser.columns == ['Configuration', 'g', 'E(cm^-1)', '10^15 Hz', 'eV', 'Lam(A)', 'ID', 'ARAD', 'C4', 'C6']
 
+@with_refdata
 def test_fevi_osc_kb_rk(fevi_osc_kb_rk_fname):
     parser = CMFGENOscillatorStrengthsParser(fevi_osc_kb_rk_fname)
     n = int(parser.meta['Number of transitions'])
@@ -58,6 +69,7 @@ def test_fevi_osc_kb_rk(fevi_osc_kb_rk_fname):
     assert parser.columns == ['State A', 'State B', 'f', 'A', 'Lam(A)', 'i', 'j', 'Lam(obs)', '% Acc']
     assert np.isclose(parser.base.iloc[0,2], 1.94e-02)
 
+@with_refdata
 def test_p2_osc(p2_osc_fname):
     parser = CMFGENOscillatorStrengthsParser(p2_osc_fname)
     n = int(parser.meta['Number of transitions'])
@@ -70,10 +82,12 @@ def test_p2_osc(p2_osc_fname):
     assert np.isclose(parser.base.iloc[2,7], 1532.51)
     assert np.isclose(parser.base.iloc[3,7], 1301.87)
 
+@with_refdata
 def test_vi_osc(vi_osc_fname):
     parser = CMFGENOscillatorStrengthsParser(vi_osc_fname)
     assert parser.base.empty
 
+@with_refdata
 def test_he2_col(he2_col_fname):
     parser = CMFGENCollisionalDataParser(he2_col_fname)
     assert parser.base.shape[0] == 465
@@ -81,11 +95,13 @@ def test_he2_col(he2_col_fname):
     assert parser.base.iloc[-1,0] == '30___'
     assert parser.base.iloc[-1,1] == 'I'
 
+@with_refdata
 def test_ariii_col(ariii_col_fname):
     parser = CMFGENCollisionalDataParser(ariii_col_fname)
     n = int(parser.meta['Number of transitions'])
     assert parser.base.shape == (n, 13)
 
+@with_refdata
 def test_si2_pho(si2_pho_fname):
     parser = CMFGENPhotoionizationCrossSectionParser(si2_pho_fname)
     n = int(parser.meta['Number of energy levels'])
@@ -93,13 +109,9 @@ def test_si2_pho(si2_pho_fname):
     assert len(parser.base) == n
     assert parser.base[0].shape == (m, 2)
 
+@with_refdata
 def test_coiv_pho(coiv_pho_fname):
     parser = CMFGENPhotoionizationCrossSectionParser(coiv_pho_fname)
     n = int(parser.meta['Number of energy levels'])
     assert len(parser.base) == n
     assert parser.base[0].shape == (3, 8)
-
-
-
-
-
