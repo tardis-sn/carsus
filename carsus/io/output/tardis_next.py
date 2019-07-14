5af2edfe4ee802a11ff25fa3da518dbf563e1be5
epassaro <epassaro15@gmail.com>
2019-07-14 18:00:31 -0300

    Added KnoxLongZetaData class

--- carsus/io/output/tardis_next.py
+++ carsus/io/output/tardis_next.py
@@ -24,3 +24,36 @@ class NISTIonizationEnergies:
     def to_hdf(self, fname):
         with pd.HDFStore(fname, 'a') as f:
             f.append('/ionization_data', self.ioniz_energies)
+
+
+class KnoxLongZetaData:  
+    def __init__(self, fname):
+        self.fname = fname
+        self._prepare_data()
+
+    def _prepare_data(self):
+        t_values = np.arange(2000, 42000, 2000)
+
+        names = ['atomic_number', 'ion_charge']
+        names += [str(i) for i in t_values]
+
+        zeta_raw = np.recfromtxt(
+                self.fname,
+                usecols=range(1, 23),
+                names=names)
+
+        self.zeta_data = (
+                pd.DataFrame(zeta_raw).set_index(
+                    ['atomic_number', 'ion_charge'])
+                )
+
+        columns = [float(c) for c in self.zeta_data.columns]
+
+        # To match exactly the `old` format
+        self.zeta_data.columns = pd.Float64Index(columns, name='temp')
+
+    def to_hdf(self, fname):
+        with pd.HDFStore(fname, 'a') as f:
+            f.append('/zeta_data', self.zeta_data)
+
+        
