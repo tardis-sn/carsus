acfd45ebe86a5eca7dd4739eadfacd198b176b30
epassaro <epassaro15@gmail.com>
2019-07-16 17:05:06 -0300

    New class for straight to HDF parse and dump

--- carsus/io/nist/__init__.py
+++ carsus/io/nist/__init__.py
@@ -1,2 +1,7 @@
-from .weightscomp import download_weightscomp, NISTWeightsCompPyparser, NISTWeightsCompIngester
-from .ionization import download_ionization_energies, NISTIonizationEnergiesParser, NISTIonizationEnergiesIngester
+from carsus.io.nist.weightscomp import (download_weightscomp, 
+                                   NISTWeightsCompPyparser, 
+                                   NISTWeightsCompIngester)
+from carsus.io.nist.ionization import (download_ionization_energies, 
+                                       NISTIonizationEnergiesParser, 
+                                       NISTIonizationEnergiesIngester,
+                                       NISTIonizationEnergies)
--- carsus/io/nist/ionization.py
+++ carsus/io/nist/ionization.py
@@ -4,6 +4,7 @@ http://physics.nist.gov/PhysRefData/ASD/ionEnergy.html
 """
 
 import requests
+import numpy as np
 import pandas as pd
 
 from bs4 import BeautifulSoup
@@ -290,3 +291,25 @@ class NISTIonizationEnergiesIngester(BaseIngester):
         if ground_levels:
             self.ingest_ground_levels()
             self.session.flush()
+
+
+class NISTIonizationEnergies:
+    """ """
+    def __init__(self, spectra):
+        input_data = download_ionization_energies(spectra)
+        self.parser = NISTIonizationEnergiesParser(input_data)
+        self._prepare_data()
+
+    def _prepare_data(self):
+        self.ioniz_energies = pd.DataFrame()
+        self.ioniz_energies['atomic_number'] = self.parser.base['atomic_number']
+        self.ioniz_energies['ion_number'] = self.parser.base['ion_charge'] + 1
+        self.ioniz_energies['ionization_energy'] = self.parser.base['ionization_energy_str'].str.strip('[]()').astype(np.float64)
+        self.ioniz_energies.set_index(['atomic_number', 'ion_number'], inplace=True)
+
+        # Convert DataFrame to Series
+        self.ioniz_energies = self.ioniz_energies['ionization_energy']
+
+    def to_hdf(self, fname):
+        with pd.HDFStore(fname, 'a') as f:
+            f.append('/ionization_data', self.ioniz_energies)
