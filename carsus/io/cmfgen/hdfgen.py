d87f892cd777b1e190f575085822c67b96edd8d3
epassaro <epassaro15@gmail.com>
2019-07-19 11:42:31 -0300

    Removed bare `except`

--- carsus/io/cmfgen/hdfgen.py
+++ carsus/io/cmfgen/hdfgen.py
@@ -27,14 +27,16 @@ def hdf_dump(cmfgen_dir, patterns, parser, chunk_size=10, ignore_patterns=[]):
                 logger.info('Parsed {}'.format(output))
                 _.append(obj)
 
-            except:
-                logger.error('Failed parsing {}'.format(output))
+            except TypeError:
+                logger.error('Failed parsing {} (try checking `find_row` function)'.format(output))
+            
+            except UnboundLocalError:
+                logger.error('Failed parsing {} (try checking `to_float` function)'.format(output))
+            
+            except IsADirectoryError:
+                logger.error('Failed parsing {} (is a directory)'.format(output))
 
         for obj in _:
             output = obj.fname.replace(cmfgen_dir, '')
-            try:
-                obj.to_hdf()
-                logger.info('Dumped {}.h5'.format(output))
-
-            except:
-                logger.error('Failed dump {}'.format(output))
+            obj.to_hdf()
+            logger.info('Dumped {}.h5'.format(output))
