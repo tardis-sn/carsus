1bdd5119adf005e4c2d87f27fcc53188b2243965
PIYUSHgoyal16 <43112419+PIYUSHgoyal16@users.noreply.github.com>
2020-03-06 16:43:59 +0530

    Added tests for Atomic_Data class

---
+++ carsus/io/tests/test_atomicdata.py
@@ -0,0 +1,29 @@
+"""
+=====================
+TARDIS Atomic_Data test module
+=====================
+created on Mar 3, 2020
+"""
+
+import numpy as np
+import pandas as pd
+from carcus.io.atomic_data import Atomic_Data
+import pytest
+
+
+def test_atomicdata():
+    
+    """
+    Creating Atomic_Data object and checking various members
+    """
+    
+    atomic_data = Atomic_Data()
+    first_table = atomic_data.first_table
+    second_table = atomic_data.second_table
+    
+    assert len(list(first_table.columns)) == 11
+    assert len(list(second_table.columns)) == 8
+    
+    assert first_table.shape[0] == 157
+    assert second_table.shape[0] == 4196
+    
