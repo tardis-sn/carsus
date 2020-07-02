b425054409adf63cb4b7d647d0f16e61d2a23a08
Ezequiel Pássaro <epassaro15@gmail.com>
2020-07-02 17:55:40 -0300

    Add test for hashing function (3)

--- carsus/util/tests/test_hash.py
+++ carsus/util/tests/test_hash.py
@@ -5,13 +5,11 @@ from carsus.util import serialize_pandas_object, hash_pandas_object
 
 
 @pytest.mark.parametrize(
-    "values, md5, sha1",
+    "values, md5",
     [
-        ([(0, 1), (1, 2), (2, 3), (3, 4)], "a703629383", "d733a2c2dc"),
-        (["apple", "banana", "orange"], "24e45baf79", "dd9a9d4b88"),
+        ([(0, 1), (1, 2), (2, 3), (3, 4)], "a703629383"),
+        (["apple", "banana", "orange"], "24e45baf79"),
     ],
 )
-def test_hash_pd(values, md5, sha1):
+def test_hash_pd(values, md5):
     assert hash_pandas_object(pd.DataFrame(values))[:10] == md5
-    assert hash_pandas_object(pd.DataFrame(values), algorithm="sha1")[:10] == sha1
-
