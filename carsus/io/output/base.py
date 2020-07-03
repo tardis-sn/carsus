35630c71d7aff2340d48d5e8f9d4730dd8abf9a7
Ezequiel Pássaro <epassaro15@gmail.com>
2020-07-03 15:09:25 -0300

    Remove unnecesary encoding

--- carsus/io/output/base.py
+++ carsus/io/output/base.py
@@ -711,8 +711,8 @@ class TARDISAtomData:
             logger.info(f"MD5: {md5_hash.hexdigest()}")
             logger.info(f"UUID1: {uuid1}")
 
-            f.root._v_attrs['md5'] = md5_hash.hexdigest().encode('ascii')
-            f.root._v_attrs['uuid1'] = uuid1.encode('ascii')
+            f.root._v_attrs['md5'] = md5_hash.hexdigest()
+            f.root._v_attrs['uuid1'] = uuid1
             f.put('/meta', meta_df)
 
             utc = pytz.timezone('UTC')
