a3ae03b6854c9e5024e72fdb0232b927f90b27c5
atharva-2001 <aryaa_1@rknec.edu>
2022-05-18 22:06:28 +0530

    ChiantiPy warns automatically. Move import code so that their warning is triggered.

--- carsus/io/__init__.py
+++ carsus/io/__init__.py
@@ -1,17 +1,10 @@
 import os
-import logging
 from carsus.io.nist import NISTIonizationEnergiesParser, NISTIonizationEnergiesIngester,\
     NISTWeightsCompPyparser, NISTWeightsCompIngester
 from carsus.io.kurucz import GFALLReader, GFALLIngester
 from carsus.io.output import AtomData
 from carsus.io.zeta import KnoxLongZetaIngester
 
-logger = logging.getLogger(__name__)    
 
 if "XUVTOP" in os.environ:
     from carsus.io.chianti_ import ChiantiIonReader, ChiantiIngester
-else:
-    logger.warning(
-        "XUVTOP environment variable is not set in your shell configuration file. "
-        "The XUVTOP environment variable is required if you want to use the Chianti submodule."
-    )
