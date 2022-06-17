114529a3c680ac04eb7066641e1a35377e41833b
atharva-2001 <aryaa_1@rknec.edu>
2022-06-17 21:29:44 +0530

    Remove unused imports

--- carsus/io/tests/test_cmfgen.py
+++ carsus/io/tests/test_cmfgen.py
@@ -2,9 +2,6 @@ import os
 import pytest
 import numpy as np
 import pandas as pd
-from io import StringIO
-from numpy.testing import assert_allclose
-from pandas.testing import assert_frame_equal
 from carsus.io.cmfgen import (CMFGENEnergyLevelsParser,
                               CMFGENOscillatorStrengthsParser,
                               CMFGENCollisionalStrengthsParser,
