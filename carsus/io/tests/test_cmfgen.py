4b37cef4af4ae2090610186c596904f0a73f9b47
atharva-2001 <aryaa_1@rknec.edu>
2022-07-12 21:42:38 +0530

    Black format

--- carsus/io/tests/test_cmfgen.py
+++ carsus/io/tests/test_cmfgen.py
@@ -2,131 +2,163 @@ import os
 import pytest
 import numpy as np
 import pandas as pd
-from carsus.io.cmfgen import (CMFGENEnergyLevelsParser,
-                              CMFGENOscillatorStrengthsParser,
-                              CMFGENCollisionalStrengthsParser,
-                              CMFGENPhoCrossSectionsParser,
-                              CMFGENHydLParser,
-                              CMFGENHydGauntBfParser,
-                              CMFGENReader
-                             )
+from carsus.io.cmfgen import (
+    CMFGENEnergyLevelsParser,
+    CMFGENOscillatorStrengthsParser,
+    CMFGENCollisionalStrengthsParser,
+    CMFGENPhoCrossSectionsParser,
+    CMFGENHydLParser,
+    CMFGENHydGauntBfParser,
+    CMFGENReader,
+)
 
 from carsus.io.cmfgen.util import *
 
 with_refdata = pytest.mark.skipif(
-    not pytest.config.getoption("--refdata"),
-    reason="--refdata folder not specified"
+    not pytest.config.getoption("--refdata"), reason="--refdata folder not specified"
 )
-data_dir = os.path.join(os.path.dirname(__file__), 'data')
+data_dir = os.path.join(os.path.dirname(__file__), "data")
+
 
 @with_refdata
 @pytest.fixture()
 def si2_osc_kurucz_fname(refdata_path):
-    return os.path.join(refdata_path, 'cmfgen', 'energy_levels', 'si2_osc_kurucz')
+    return os.path.join(refdata_path, "cmfgen", "energy_levels", "si2_osc_kurucz")
+
 
 @with_refdata
 @pytest.fixture()
 def si2_col_fname(refdata_path):
-    return os.path.join(refdata_path, 'cmfgen', 'collisional_strengths', 'si2_col')
+    return os.path.join(refdata_path, "cmfgen", "collisional_strengths", "si2_col")
+
 
 @with_refdata
 @pytest.fixture()
 def si1_data_dict(si2_osc_kurucz_fname, si2_col_fname):
-    si1_levels = CMFGENEnergyLevelsParser(si2_osc_kurucz_fname).base  #  (carsus) Si 1 == Si II
+    si1_levels = CMFGENEnergyLevelsParser(
+        si2_osc_kurucz_fname
+    ).base  #  (carsus) Si 1 == Si II
     si1_lines = CMFGENOscillatorStrengthsParser(si2_osc_kurucz_fname).base
     si1_col = CMFGENCollisionalStrengthsParser(si2_col_fname).base
-    return {(14,1): dict(levels = si1_levels, lines = si1_lines, collisions = si1_col)}
+    return {(14, 1): dict(levels=si1_levels, lines=si1_lines, collisions=si1_col)}
+
 
 @with_refdata
 @pytest.fixture()
 def si1_reader(si1_data_dict):
     return CMFGENReader(si1_data_dict, collisions=True)
 
+
 @pytest.fixture()
 def cmfgen_refdata_fname(refdata_path, path):
     subdirectory, fname = path
     return os.path.join(refdata_path, "cmfgen", subdirectory, fname)
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
-@pytest.mark.parametrize('path', [
-    ['energy_levels', 'si2_osc_kurucz'],
-])
+@pytest.mark.array_compare(file_format="pd_hdf")
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["energy_levels", "si2_osc_kurucz"],
+    ],
+)
 def test_CMFGENEnergyLevelsParser(cmfgen_refdata_fname):
     parser = CMFGENEnergyLevelsParser(cmfgen_refdata_fname)
-    n = int(parser.header['Number of energy levels'])
+    n = int(parser.header["Number of energy levels"])
     assert parser.base.shape[0] == n
     return parser.base
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
-@pytest.mark.parametrize('path', [
-    ['oscillator_strengths', 'fevi_osc_kb_rk.dat'],
-    ['oscillator_strengths', 'p2_osc'],
-    ['oscillator_strengths', 'vi_osc']
-])
+@pytest.mark.array_compare(file_format="pd_hdf")
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["oscillator_strengths", "fevi_osc_kb_rk.dat"],
+        ["oscillator_strengths", "p2_osc"],
+        ["oscillator_strengths", "vi_osc"],
+    ],
+)
 def test_CMFGENOscillatorStrengthsParser(cmfgen_refdata_fname):
     parser = CMFGENOscillatorStrengthsParser(cmfgen_refdata_fname)
-    n = int(parser.header['Number of transitions'])
+    n = int(parser.header["Number of transitions"])
     assert parser.base.shape[0] == n
     return parser.base
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
-@pytest.mark.parametrize('path', [
-    ['collisional_strengths', 'he2col.dat'],
-    ['collisional_strengths', 'col_ariii'],
-])
+@pytest.mark.array_compare(file_format="pd_hdf")
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["collisional_strengths", "he2col.dat"],
+        ["collisional_strengths", "col_ariii"],
+    ],
+)
 def test_CMFGENCollisionalStrengthsParser(cmfgen_refdata_fname):
     parser = CMFGENCollisionalStrengthsParser(cmfgen_refdata_fname)
     return parser.base
 
 
 @with_refdata
-@pytest.mark.parametrize('path', [
-    ['photoionization_cross_sections', 'phot_nahar_A'],
-    ['photoionization_cross_sections', 'phot_data_gs'],
-])
-@pytest.mark.array_compare(file_format='pd_hdf')
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["photoionization_cross_sections", "phot_nahar_A"],
+        ["photoionization_cross_sections", "phot_data_gs"],
+    ],
+)
+@pytest.mark.array_compare(file_format="pd_hdf")
 def test_CMFGENPhoCrossSectionsParser(cmfgen_refdata_fname):
     parser = CMFGENPhoCrossSectionsParser(cmfgen_refdata_fname)
-    n = int(parser.header['Number of energy levels'])
+    n = int(parser.header["Number of energy levels"])
     assert len(parser.base) == n
     return parser.base[0]
 
 
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
-@pytest.mark.parametrize('path', [
-    [ "photoionization_cross_sections", "hyd_l_data.dat"],
-])
+@pytest.mark.array_compare(file_format="pd_hdf")
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["photoionization_cross_sections", "hyd_l_data.dat"],
+    ],
+)
 def test_CMFGENHydLParser(cmfgen_refdata_fname):
     parser = CMFGENHydLParser(cmfgen_refdata_fname)
     assert parser.header["Maximum principal quantum number"] == "30"
     return parser.base
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
-@pytest.mark.parametrize('path', [
-    [ "photoionization_cross_sections", "gbf_n_data.dat"],
-])
+@pytest.mark.array_compare(file_format="pd_hdf")
+@pytest.mark.parametrize(
+    "path",
+    [
+        ["photoionization_cross_sections", "gbf_n_data.dat"],
+    ],
+)
 def test_CMFGENHydGauntBfParser(cmfgen_refdata_fname):
     parser = CMFGENHydGauntBfParser(cmfgen_refdata_fname)
     assert parser.header["Maximum principal quantum number"] == "30"
     return parser.base
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
+@pytest.mark.array_compare(file_format="pd_hdf")
 def test_reader_lines(si1_reader):
     return si1_reader.lines
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
+@pytest.mark.array_compare(file_format="pd_hdf")
 def test_reader_levels(si1_reader):
     return si1_reader.levels
 
+
 @with_refdata
-@pytest.mark.array_compare(file_format='pd_hdf')
+@pytest.mark.array_compare(file_format="pd_hdf")
 def test_reader_collisions(si1_reader):
     return si1_reader.collisions
 
@@ -178,27 +210,21 @@ def test_get_hydrogenic_nl_phixs_table(
 
 @pytest.mark.array_compare
 @pytest.mark.parametrize("threshold_energy_ryd", [2])
-@pytest.mark.parametrize("vars", [[3,4,5,6,7]])
+@pytest.mark.parametrize("vars", [[3, 4, 5, 6, 7]])
 @pytest.mark.parametrize("n_points", [50])
-def test_get_opproject_phixs_table(
-    threshold_energy_ryd, vars, n_points
-):
-    phixs_table = get_opproject_phixs_table(
-        threshold_energy_ryd, *vars, n_points
-    )
+def test_get_opproject_phixs_table(threshold_energy_ryd, vars, n_points):
+    phixs_table = get_opproject_phixs_table(threshold_energy_ryd, *vars, n_points)
     return phixs_table
+
 
 @pytest.mark.array_compare
 @pytest.mark.parametrize("threshold_energy_ryd", [2])
-@pytest.mark.parametrize("vars", [[2,3,4,5,6,7,8]])
+@pytest.mark.parametrize("vars", [[2, 3, 4, 5, 6, 7, 8]])
 @pytest.mark.parametrize("n_points", [50])
-def test_get_hummer_phixs_table(
-    threshold_energy_ryd, vars, n_points
-):
-    phixs_table = get_hummer_phixs_table(
-        threshold_energy_ryd, *vars, n_points
-    )
+def test_get_hummer_phixs_table(threshold_energy_ryd, vars, n_points):
+    phixs_table = get_hummer_phixs_table(threshold_energy_ryd, *vars, n_points)
     return phixs_table
+
 
 @pytest.mark.array_compare
 @pytest.mark.parametrize("threshold_energy_ryd", [10])
@@ -220,19 +246,17 @@ def test_get_hummer_phixs_table(
 )
 @pytest.mark.parametrize("n_points", [50])
 def test_get_vy95_phixs_table(threshold_energy_ryd, fit_coeff_table, n_points):
-    phixs_table = get_vy95_phixs_table(
-        threshold_energy_ryd, fit_coeff_table, n_points
-    )
+    phixs_table = get_vy95_phixs_table(threshold_energy_ryd, fit_coeff_table, n_points)
     return phixs_table
+
 
 @pytest.mark.skip(reason="Not implemented yet")
 def test_get_leibowitz_phixs_table():
     pass
+
 
 @pytest.mark.array_compare
 @pytest.mark.parametrize("threshold_energy_ryd", [50])
 def test_get_null_phixs_table(threshold_energy_ryd):
     phixs_table = get_null_phixs_table(threshold_energy_ryd)
     return phixs_table
-
-
