b05ec39a225e1f3824cd6a9561b17002bd2dec1e
shreyas3156 <shreyashsingh31@gmail.com>
2023-07-17 15:42:55 +0530

    Add unit test for _prepare_nuclear_dataframes()

--- carsus/io/tests/test_nndc.py
+++ carsus/io/tests/test_nndc.py
@@ -40,6 +40,26 @@ def test_get_nuclear_dataframe(nndc_reader, element, z, parent_e_level, decay_mo
                              ("Ni56", "Ni", 28, 0.0, False, "EC", 100.00, "g", "XR ka2", 6.915),
                              ("Mn52", "Mn", 25, 377.749, True, "IT", 1.75, "g", "XR ka1", 5.899)
                          ])
+def test_prepare_nuclear_dataframes(nndc_reader, index, element, z, parent_e_level, metastable,
+                                    decay_mode, decay_mode_value, radiation, rad_subtype, rad_energy):
+    decay_data = NNDCReader._prepare_nuclear_dataframes(nndc_reader)
+    df = decay_data[(decay_data.index == index) & (decay_data["Decay Mode"] == decay_mode) &
+                    (decay_data["Decay Mode Value"] == decay_mode_value) &
+                    (decay_data["Rad subtype"] == rad_subtype)]
+
+    row = df.loc[index]
+    assert row["Element"] == element
+    assert row["Z"] == z
+    assert row["Parent E(level)"] == parent_e_level
+    assert row["Radiation"] == radiation
+    assert row["Rad Energy"] == rad_energy
+
+
+@pytest.mark.parametrize("index, element, z, parent_e_level, metastable, "
+                         "decay_mode, decay_mode_value, radiation, rad_subtype, rad_energy", [
+                             ("Ni56", "Ni", 28, 0.0, False, "EC", 100.00, "g", "XR ka2", 6.915),
+                             ("Mn52", "Mn", 25, 377.749, True, "IT", 1.75, "g", "XR ka1", 5.899)
+                         ])
 def test_nndc_reader_decay_data(decay_data, index, element, z, parent_e_level, metastable,
                                 decay_mode, decay_mode_value, radiation, rad_subtype, rad_energy):
     df = decay_data[(decay_data.index == index) & (decay_data["Decay Mode"] == decay_mode) &
