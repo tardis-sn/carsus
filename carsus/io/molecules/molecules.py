696470dc2c425518f7403180197f20d1b0e013b2
Josh Shields <shields.joshua.v@gmail.com>
2024-08-15 16:10:08 -0400

    add full barklem2016 reader

--- carsus/io/molecules/molecules.py
+++ carsus/io/molecules/molecules.py
@@ -7,22 +7,44 @@ from pathlib import Path
 
 BARKLEM_COLETT_DATA_URL = "https://raw.githubusercontent.com/tardis-sn/carsus-data-molecules-barklem2016/main/data/"
 
-
 logger = logging.getLogger(__name__)
 
 
 class BarklemColett2016Reader(object):
+    """
+    A class for reading and parsing data from the Barklem & Collet 2016 dataset.
+
+    This class initializes with a file path and provides methods to read raw data and parse it into structured dataframes for dissociation energies, ionization energies, partition functions, and equilibrium constants. It also manages versioning information for each dataset.
+
+    Args:
+        fpath (str, optional): The file path to the data source. If not provided, defaults to BARKLEM_COLETT_DATA_URL.
+
+    Attributes:
+        barklem_2016_raw (tuple): Raw data from the Barklem & Collet 2016 dataset as a tuple of dataframes and checksums.
+        dissociation_energies (DataFrame): Dataframe containing dissociation energies.
+        ionization_energies (DataFrame): Dataframe containing ionization energies.
+        partition_functions (DataFrame): Dataframe containing partition functions.
+        equilibrium_constants (DataFrame): Dataframe containing equilibrium constants.
+        dissociation_version (str): Version information for dissociation energies.
+        ionization_version (str): Version information for ionization energies.
+        partition_version (str): Version information for partition functions.
+        equilibrium_version (str): Version information for equilibrium constants.
+    """
 
     def __init__(self, fpath=None):
 
         self.fpath = BARKLEM_COLETT_DATA_URL if fpath is None else fpath
         self._barklem_2016_raw = None
-        self._barklem_2016 = None
 
-        self.dissociation_energies = None
-        self.ionization_energies = None
-        self.partition_functions = None
-        self.equilibrium_constants = None
+        self._dissociation_energies = None
+        self._ionization_energies = None
+        self._partition_functions = None
+        self._equilibrium_constants = None
+
+        self.dissociation_version = None
+        self.ionization_version = None
+        self.partition_version = None
+        self.equilibrium_version = None
 
     @property
     def barklem_2016_raw(self):
@@ -34,7 +56,25 @@ class BarklemColett2016Reader(object):
         return self._barklem_2016_raw
 
     def read_barklem_2016_raw(self, fpath=None):
+        """
+        Reads raw data from the Barklem & Collet 2016 dataset and returns structured dataframes.
 
+        This function retrieves data from specified files related to dissociation energies, ionization energies, partition functions, and equilibrium constants. It processes the data into pandas dataframes and returns them along with their respective checksums for verification.
+
+        Args:
+            fpath (str, optional): The file path to the data source. If not provided, defaults to the instance's fpath attribute.
+
+        Returns:
+            tuple: A tuple containing:
+                - dissociation_energies_df (DataFrame): Dataframe of dissociation energies.
+                - ionization_energies_df (DataFrame): Dataframe of ionization energies.
+                - partition_functions_df (DataFrame): Dataframe of partition functions.
+                - equilibrium_constants_df (DataFrame): Dataframe of equilibrium constants.
+                - diss_checksum (str): Checksum for the dissociation energies data.
+                - ioniz_checksum (str): Checksum for the ionization energies data.
+                - part_checksum (str): Checksum for the partition functions data.
+                - equil_checksum (str): Checksum for the equilibrium constants data.
+        """
         if fpath is None:
             fpath = self.fpath
 
@@ -45,7 +85,7 @@ class BarklemColett2016Reader(object):
         partition_functions_data = f"{fpath}table6.dat"
         equilibrium_constants_data = f"{fpath}table7.dat"
 
-        diss_buffer, checksum = read_from_buffer(dissociation_energies_data)
+        diss_buffer, diss_checksum = read_from_buffer(dissociation_energies_data)
         dissociation_energies_df = pd.read_csv(
             diss_buffer,
             delimiter=r"\s+",
@@ -67,7 +107,7 @@ class BarklemColett2016Reader(object):
             index_col=0,
         )
 
-        ioniz_buffer, checksum = read_from_buffer(ionization_energies_data)
+        ioniz_buffer, ioniz_checksum = read_from_buffer(ionization_energies_data)
         ionization_energies_df = pd.read_csv(
             ioniz_buffer,
             delimiter=r"\s+",
@@ -77,7 +117,7 @@ class BarklemColett2016Reader(object):
             index_col=0,
         )
 
-        part_buffer, checksum = read_from_buffer(partition_functions_data)
+        part_buffer, part_checksum = read_from_buffer(partition_functions_data)
         partition_functions_df = pd.read_csv(
             part_buffer,
             delimiter=r"\s{2,}",
@@ -89,9 +129,9 @@ class BarklemColett2016Reader(object):
         partition_functions_df.index.name = "Molecule"
         partition_functions_df.columns = partition_functions_df.columns.astype(float)
 
-        eq_buffer, checksum = read_from_buffer(equilibrium_constants_data)
+        equil_buffer, equil_checksum = read_from_buffer(equilibrium_constants_data)
         equilibrium_constants_df = pd.read_csv(
-            eq_buffer,
+            equil_buffer,
             delimiter=r"\s{2,}",
             skiprows=[0, 1, 3],
             escapechar="#",
@@ -108,5 +148,70 @@ class BarklemColett2016Reader(object):
             ionization_energies_df,
             partition_functions_df,
             equilibrium_constants_df,
-            checksum,
+            diss_checksum,
+            ioniz_checksum,
+            part_checksum,
+            equil_checksum,
         )
+
+    def parse_barklem_2016(self):
+        """
+        Parses raw barklem2016 data
+        """
+
+        (
+            dissociation_energies_df,
+            ionization_energies_df,
+            partition_functions_df,
+            equilibrium_constants_df,
+            dissociation_version,
+            ionization_version,
+            partition_version,
+            equilibrium_version,
+        ) = self.barklem_2016_raw
+
+        self._dissociation_energies = dissociation_energies_df
+        self._ionization_energies = ionization_energies_df
+        self._partition_functions = partition_functions_df
+        self._equilibrium_constants = equilibrium_constants_df
+
+        self.dissociation_version = dissociation_version
+        self.ionization_version = ionization_version
+        self.partition_version = partition_version
+        self.equilibrium_version = equilibrium_version
+
+    @property
+    def dissociation_energies(self):
+        """
+        Returns the dissociation energies dataframe
+        """
+        if self._dissociation_energies is None:
+            self.parse_barklem_2016()
+        return self._dissociation_energies
+
+    @property
+    def ionization_energies(self):
+        """
+        Returns the ionization energies dataframe
+        """
+        if self._ionization_energies is None:
+            self.parse_barklem_2016()
+        return self._ionization_energies
+
+    @property
+    def partition_functions(self):
+        """
+        Returns the partition functions dataframe
+        """
+        if self._partition_functions is None:
+            self.parse_barklem_2016()
+        return self._partition_functions
+
+    @property
+    def equilibrium_constants(self):
+        """
+        Returns the equilibrium constants dataframe
+        """
+        if self._equilibrium_constants is None:
+            self.parse_barklem_2016()
+        return self._equilibrium_constants
