8f39ea768d94badf64d42d0af3433c9f98d73eec
Andrew Fullard <andrewgfullard@gmail.com>
2024-08-19 16:23:34 -0400

    Tiny bit of pathlib

--- carsus/base.py
+++ carsus/base.py
@@ -1,9 +1,7 @@
-import os
+from pathlib import Path
 import logging
 import carsus
 
 logger = logging.getLogger(__name__)
 
-basic_atomic_data_fname = os.path.join(
-    carsus.__path__[0], "data", "basic_atomic_data.csv"
-)
+basic_atomic_data_fname = Path(carsus.__path__[0]) / "data" / "basic_atomic_data.csv"
