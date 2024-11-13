1e99b0f67bc6798be0c2f04adc6eed9d50497bac
Josh Shields <shields.joshua.v@gmail.com>
2024-11-13 12:41:07 -0500

    apply formatting

--- carsus/io/tests/test_vald.py
+++ carsus/io/tests/test_vald.py
@@ -65,9 +65,7 @@ def test_vald_reader_vald_raw(vald_raw, index, wl_air, log_gf, e_low, e_up):
     ],
 )
 def test_vald_reader_vald(vald, index, wl_air, log_gf, e_low, e_up, ion_charge):
-    row = vald[
-        1
-    ].loc[
+    row = vald[1].loc[
         index
     ]  # vald is a length two array. The first element is the atoms and the second is the molecules
     assert_almost_equal(row["WL_air(A)"], wl_air)
